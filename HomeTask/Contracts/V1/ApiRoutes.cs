1a30d98ceb7763d5aedf26ca672d8e598192668d
Данил <danil-kuptsov@yandex.ru>
2020-01-27 23:40:51 +0300

    1

---
+++ HomeTask/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeTask.Contracts.V1
+{
+
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+
+        public const string Version = "v1";
+
+        public const string Base = Root + "/" + Version;
+
+        public static class Todo
+        {
+            public const string GetAll = Base + "/todo";
+
+            public const string Update = Base + "/todo/{todoid}";
+
+            public const string Delete = Base + "/todo/{todoid}";
+
+            public const string Get = Base + "/todo/{todoid}";
+
+            public const string Create = Base + "/todo";
+        }
+
+        public static class Identity
+        {
+            public const string Login = Base + "/identity/login";
+
+            public const string Register = Base + "/identity/register";
+
+            public const string Refresh = Base + "/identity/refresh";
+        }
+    }
+
+}
